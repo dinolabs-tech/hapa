8dcbb0e152c6be2c1180ab745bd52ba3fe2ff41c
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2025-12-11 13:04:26 +0100

    debug

--- portal/admincheckresult.php
+++ portal/admincheckresult.php
@@ -212,20 +212,20 @@ while ($avg_row = $subject_averages_result->fetch_assoc()) {
 
 
 $pos_query = $conn->query("
-    SELECT *
-    FROM (
-        SELECT
-            id,
-            SUM(total) AS overall_total,
-            RANK() OVER (ORDER BY SUM(total) DESC) AS position
-        FROM mastersheet
-        WHERE class = '{$student_details['class']}'
-        AND arm = '{$student_details['arm']}'
-          AND term = '$term'
-          AND csession = '$curr_session'
-        GROUP BY id
-    ) AS ranked
-    WHERE id = '$student_id'
+  SELECT *
+FROM (
+    SELECT
+        id,
+        SUM(CAST(TRIM(total) AS UNSIGNED)) AS overall_total,
+        RANK() OVER (ORDER BY SUM(CAST(TRIM(total) AS UNSIGNED)) DESC) AS position
+    FROM mastersheet
+    WHERE class = '{$student_details['class']}'
+      AND arm = '{$student_details['arm']}'
+      AND term = '$term'
+      AND csession = '$curr_session'
+    GROUP BY id
+) AS ranked
+WHERE id = '$student_id'
 ");
 
 // SELECT *
@@ -235,13 +235,13 @@ $pos_query = $conn->query("
 //         SUM(CAST(TRIM(total) AS UNSIGNED)) AS overall_total,
 //         RANK() OVER (ORDER BY SUM(CAST(TRIM(total) AS UNSIGNED)) DESC) AS position
 //     FROM mastersheet
-//     WHERE class = 'JSS 2'
-//       AND arm = 'A'
-//       AND term = '1st Term'
-//       AND csession = '2025/2026'
+//     WHERE class = '{$student_details['class']}'
+//       AND arm = '{$student_details['arm']}'
+//       AND term = '$term'
+//       AND csession = '$curr_session'
 //     GROUP BY id
 // ) AS ranked
-// WHERE id = '25/JS/014';
+// WHERE id = '$student_id';
 
 
 $position_row = $pos_query->fetch_assoc();
