7375e18da3e9d73178d15b05350d847c03f58062
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2026-04-02 11:48:43 +0100

    fix(portal): update student modify link to modifystudents.php
    Changed the navigation link href from `editstudents.php` to `modifystudents.php` to reflect the correct file path for the student modification page.

--- portal/adminnav.php
+++ portal/adminnav.php
@@ -62,7 +62,7 @@
                   </a>
                 </li>
                 <li>
-                  <a href="editstudents.php">
+                  <a href="modifystudents.php">
                     <span class="sub-item">Modify</span>
                   </a>
                 </li>
