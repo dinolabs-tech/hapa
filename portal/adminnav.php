fe4add18972a6a5579718a4921b2659cbc13cfe3
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2026-04-02 12:43:18 +0100

    debug

--- portal/adminnav.php
+++ portal/adminnav.php
@@ -62,7 +62,7 @@
                   </a>
                 </li>
                 <li>
-                  <a href="students_prof.php">
+                  <a href="modifystudents.php">
                     <span class="sub-item">Modify</span>
                   </a>
                 </li>
